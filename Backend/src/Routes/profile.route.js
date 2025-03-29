b72e30eb48c01ae798b467e13ce44cefbfe08ea7
Sketch <siteshkjha9@gmail.com>
2025-03-29 12:52:41 +0530

    Add: Profile update api is added

--- Backend/src/Routes/profile.route.js
+++ Backend/src/Routes/profile.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const profileRouter = express.Router();
 const userAuth = require("../middleware/Auth");
+const User = require ("../model/user.schema")
 
 profileRouter.get('/profile/view',userAuth, async (req, res) => {
     try {
@@ -17,5 +18,28 @@ profileRouter.get('/profile/view',userAuth, async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+profileRouter.get('/profile/edit',userAuth, async (req, res) => {
+        try {
+            const userId = req.user._id;
+            const updateData = req.body;
+          const  allowedData = ["firstName", "lastName", "about","photoUrl"];
+          const isAllowedData = Object.keys(req.body).every((key) => allowedData.includes(key));
+          if(!isAllowedData){
+            return res.status(400).json({ error: "Invalid data provided" });
+          }
+          const user = await User.findByIdAndUpdate(userId, updateData , { new: true });
+          if (!user) {
+            return res.status(404).json({ message: "User not found" });
+          }
+          res.json({
+            message: "User Profile Updated",
+            user
+          });
+        } catch (error) {
+            console.error("Internal Server Error:", error.message);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+})
 
 module.exports = profileRouter;
